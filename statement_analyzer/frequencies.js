0942105df9af1fbdfeda33f9bd4e0f0d7cab274d
Joshua Weinstein <jweinst1@berkeley.edu>
2015-12-02 16:00:27 -0800

    statistical functions added

--- statement_analyzer/frequencies.js
+++ statement_analyzer/frequencies.js
@@ -1,5 +1,39 @@
 /**
  * Created by Josh on 12/2/15.
  */
+//File that implements statistical frequencies for words in text.
 
-//
+//returns frequency of word in text
+function word_freq(text, word) {
+    var words = text.split(" ");
+    var count = 0;
+    for(i=0;i<words.length;i++) if(words[i]==word) count++;
+    return count/words.length;
+}
+//creates an object dict of all word frequencies in a piece of text.
+function all_frequencies(text) {
+    var words = text.split(" ");
+    for(var num in words) this[words[num]] = word_freq(text, words[num]);
+}
+
+var freq_opers = {
+    max : function (freqdict) {
+        var current = Object.keys(freqdict)[0];
+        for(var key in freqdict) {
+            if(freqdict[key] > freqdict[current]) current = key;
+        }
+        return current;
+    },
+    min : function (freqdict) {
+        var current = Object.keys(freqdict)[0];
+        for(var key in freqdict) {
+            if(freqdict[key] < freqdict[current]) current = key;
+        }
+        return current;
+    },
+    f_range : function (freqdict) {
+        return freq_opers.max(freqdict) - freq_opers.min(freqdict);
+    }
+};
+
+
